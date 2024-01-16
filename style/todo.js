443ad28db14dba4b0f9b65c5d6fe2e425f78d11f
Bemadler <akerovab@gmail.com>
2024-01-16 21:18:49 +0600

    commit changes and updates
    changes

--- style/todo.js
+++ style/todo.js
@@ -1,6 +1,6 @@
-// Функция для добавления новой задачи
+
 function addNewTask(event) {
-    event.preventDefault(); // Предотвращаем отправку формы
+    event.preventDefault(); 
 
     const taskInput = document.getElementById('taskInput');
     const taskText = taskInput.value.trim();
@@ -8,6 +8,8 @@ function addNewTask(event) {
     if (taskText !== '') {
         createTaskElement(taskText);
         taskInput.value = '';
+    } else {
+        alert('Вы не ввели дело в список');
     }
 }
 
@@ -25,18 +27,60 @@ function createTaskElement(taskText) {
     checkbox.addEventListener('change', function () {
         if (this.checked) {
             taskTextElement.style.textDecoration = 'line-through';
+            moveTaskToFinished(taskContainer);
         } else {
             taskTextElement.style.textDecoration = 'none';
         }
     });
 
-    // Добавляем текст к задаче
     taskTextElement.textContent = taskText;
-
-    // Добавляем элементы к контейнеру задачи
+    taskTextElement.style.color = '#f14377';
     taskContainer.appendChild(checkbox);
     taskContainer.appendChild(taskTextElement);
 
-    // Добавляем контейнер задачи в область задач
     taskArea.appendChild(taskContainer);
 }
+
+// перемещение задачи в завершенные
+
+function moveTaskToFinished(taskContainer) {
+    const finishedTasksArea = document.querySelector('.finishedTasks .gif-container');
+    finishedTasksArea.style.display = 'block'; 
+
+    setTimeout(function () {
+        finishedTasksArea.style.display = 'none'; 
+        const taskText = taskContainer.querySelector('span').textContent;
+
+        // Создаем элемент в завершенных
+        const finishedTaskElement = document.createElement('div');
+        finishedTaskElement.textContent = taskText;
+
+        // Добавляем элемент в область завершенных задач
+        finishedTasksArea.parentNode.insertBefore(finishedTaskElement, finishedTasksArea);
+        
+        // Удаляем задачу из области задач
+        taskContainer.remove();
+
+        const clearButton = document.getElementById('clearButton');
+        clearButton.style.display = 'block';
+    }, 1500);
+}
+
+// Обработчик для кнопки "Очистить"
+document.getElementById('clearButton').addEventListener('click', function () {
+    const finishedTasksArea = document.querySelector('.finishedTasks .gif-container');
+    const clearButton = document.getElementById('clearButton');
+
+    if (finishedTasksArea.parentNode.children.length > 1) {
+        
+        finishedTasksArea.parentNode.innerHTML = '';
+    } else {
+    
+        finishedTasksArea.style.display = 'block';
+        setTimeout(function () {
+            finishedTasksArea.style.display = 'none'; 
+        }, 2000);
+    }
+
+    clearButton.style.display = 'block';
+});
